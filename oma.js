d6feba147274a22ee8785efaf083c3047060c9c7
tiinaback <tiina.backman@myy.haaga-helia.fi>
2024-09-22 10:39:35 +0300

    finished

---
+++ oma.js
@@ -0,0 +1,37 @@
+fetch('https://run.mocky.io/v3/3e998bf7-2ef0-4b5e-9247-8db80d81327b')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(responseJson) {
+    kerro(responseJson);
+  })
+  .catch(function(error) {
+    document.getElementById("vastaus").innerHTML = "<p>Tietoa ei pystytä hakemaan</p>";
+  });
+
+
+function kerro(data) {
+  var teksti = ""; 
+
+  teksti += "<h1>" + data.otsikko + "</h1>";
+ 
+  teksti += "<p>" + data.kuvaus + "</p>";
+
+  teksti += "<p><img src='" + data.kuva + "' alt='kuva'></p>";
+
+  teksti += "<h3>Opintojakso: " + data.opintojakso.nimi + "</h3>";
+  teksti += "<p>Tunnus: " + data.opintojakso.tunnus + "</p>";
+  teksti += "<p>Opintopisteet: " + data.opintojakso.opintopisteet + "</p>";
+
+  teksti += "<ul>";
+  for (var i = 0; i < data.sisalto.length; i++) {
+    teksti += "<li>" + data.sisalto[i] + "</li>";
+  }
+  teksti += "</ul>";
+
+  for (var i = 0; i < data.tekniikat.length; i++) {
+    teksti += "<p><a href='" + data.tekniikat[i].linkki + "'>" + data.tekniikat[i].aihe + "</a></p>";
+  }
+
+  document.getElementById("vastaus").innerHTML = teksti;
+}
